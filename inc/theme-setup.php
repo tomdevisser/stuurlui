e85319358ef126cefa41b0f1236deaa762643e6c
Thomas de Visser <thomasdevisser@pm.me>
2023-01-21 19:56:58 +0100

    Added the header template part, and updated index

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -15,3 +15,15 @@ function strl_block_course_theme_unregister_patterns() {
 	remove_theme_support( 'core-block-patterns' );
 }
 add_action( 'after_setup_theme', 'strl_block_course_theme_unregister_patterns', 15 );
+
+/**
+ * Allow SVG uploads.
+ *
+ * @param array $t Mime types keyed by the file extension regex corresponding to those types.
+ * @package strl
+ */
+function strl_mime_types( $t ) {
+	$t['svg'] = 'image/svg+xml';
+	return $t;
+}
+add_filter( 'upload_mimes', 'strl_mime_types' );
