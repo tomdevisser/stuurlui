026e057e23f034370e48d6fbbb7e63fb1f0c6eff
Thomas de Visser <thomasdevisser@pm.me>
2023-01-24 18:31:15 +0000

    Started making a post selector in a block

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,7 @@ const defaultConfig = require("@wordpress/scripts/config/webpack.config.js");
 module.exports = {
 	...defaultConfig,
 	entry: {
-		pretitle: path.resolve(__dirname, "/blocks/pretitle", "index.js"),
-		services: path.resolve(__dirname, "/blocks/services", "index.js"),
-		crew: path.resolve(__dirname, "/blocks/crew", "index.js"),
-		crewmember: path.resolve(__dirname, "/blocks/crewmember", "index.js"),
-		textmedia: path.resolve(__dirname, "/blocks/text-media", "index.js"),
+		service: path.resolve(__dirname, "/blocks/service", "index.js"),
 	},
 	output: {
 		path: path.resolve(__dirname, "build"),
